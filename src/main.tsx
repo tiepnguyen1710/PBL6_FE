9b4ffece589a0b5f674e32765a846c7f53502e53
TiepNguyen <tiepnguyen.17102003@gmail.com>
2024-09-28 15:49:32 +0700

    structure 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Part1 from './features/toeic-exam/containers/Part1/Part1.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App/>
+    ),
+  },
+  {
+    path: "exam",
+    element: (
+      <Part1/>
+    )
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
