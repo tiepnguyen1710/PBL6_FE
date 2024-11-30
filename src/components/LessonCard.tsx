99fb4c5e2604bdf2aea29dbac07a9bf622dc2718
TienZe <hertyv72@gmail.com>
2024-11-30 22:54:40 +0700

    Create personal folder page

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps, Typography } from "@mui/material";
+import { Box, BoxProps, SxProps, Typography } from "@mui/material";
 import React from "react";
 
 export const LessonCard: React.FC<
@@ -7,8 +7,9 @@ export const LessonCard: React.FC<
     image: string;
     onClickCard?: (e: React.MouseEvent<HTMLDivElement>) => void;
     cardRef?: React.RefObject<HTMLDivElement>;
+    nameSx?: SxProps;
   } & BoxProps
-> = ({ name, image, sx, onClickCard, cardRef, ...rest }) => {
+> = ({ name, image, sx, onClickCard, cardRef, nameSx, ...rest }) => {
   return (
     <Box sx={{ width: "173px", overflow: "hidden", ...sx }} {...rest}>
       <Box
@@ -33,6 +34,7 @@ export const LessonCard: React.FC<
           textTransform: "uppercase",
           marginTop: "12px",
           textAlign: "center",
+          ...nameSx,
         }}
       >
         {name}
