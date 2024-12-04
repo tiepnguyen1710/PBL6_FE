360825217b1d1a0ab397f39356767e0a665e1108
TienZe <hertyv72@gmail.com>
2024-12-05 00:06:37 +0700

    Rate a voca set

--- src/features/voca/api/voca-domain.ts
+++ src/features/voca/api/voca-domain.ts
@@ -1,9 +1,29 @@
 import axiosClient from "../../../axios";
 import { VocaSetWithUserProgress } from "../../../types/VocaSetModel";
+import { VocaSetRating } from "../types/VocaSetRating";
+import { PostVocaSetRatingRequest } from "../types/VocaSetRequest";
 
 export async function getVocaSetWithUserProgress(vocaSetId: string) {
   const response = await axiosClient.get<VocaSetWithUserProgress>(
     `group-topic/${vocaSetId}/user`,
+  );
+
+  return response.data;
+}
+
+export async function postVocaSetRating(request: PostVocaSetRatingRequest) {
+  const { vocaSetId, ...data } = request;
+  const response = await axiosClient.post(
+    `rating/groupTopic/${vocaSetId}`,
+    data,
+  );
+
+  return response.data;
+}
+
+export async function getVocaSetRating(vocaSetId: string) {
+  const response = await axiosClient.get<VocaSetRating[]>(
+    `rating/groupTopic/${vocaSetId}`,
   );
 
   return response.data;
