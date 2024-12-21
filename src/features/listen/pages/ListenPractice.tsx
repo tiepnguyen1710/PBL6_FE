0266adb5eed21e70f813516aa12f0663599b2f59
dattrungnam <datvtrg0510@gmail.com>
2024-12-22 00:56:53 +0700

    feat: add overview listen practice

--- src/features/listen/pages/ListenPractice.tsx
+++ src/features/listen/pages/ListenPractice.tsx
@@ -15,12 +15,15 @@ import Content from "../../../components/layout/Content";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import TipsAndUpdatesIcon from "@mui/icons-material/TipsAndUpdates";
+import DoneIcon from "@mui/icons-material/Done";
 import { getListenLessonById } from "../api/ListenLessonApi";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 const ListenPractice: React.FC = () => {
   const { lessonId } = useParams();
   const [currIndex, setCurrIndex] = useState(1);
   const [showAnswer, setShowAnswer] = useState(false);
   const [answer, setAnswer] = useState([""]);
+  const [mode, setMode] = useState<"practice" | "overview">("practice");
   const [answerState, setAnswerState] = useState<
     { state: "success" | "error" | "info"; checked: boolean }[]
   >([
@@ -213,189 +216,337 @@ const ListenPractice: React.FC = () => {
               gap: "80px",
             }}
           >
-            <Card
-              sx={{
-                width: "80%",
-                marginLeft: "80px",
-                height: "70%",
-                minHeight: "480px",
-                boxShadow:
-                  "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
-                padding: "30px 20px",
-              }}
-            >
-              <Breadcrumbs
+            {mode === "practice" ? (
+              <Card
                 sx={{
-                  marginLeft: "20px",
-                  fontSize: "12px",
-                  marginBottom: "10px",
-                  color: "black",
-                  textDecoration: "none",
-                  display: "flex",
-                  alignItems: "center",
+                  width: "80%",
+                  marginLeft: "80px",
+                  height: "70%",
+                  minHeight: "480px",
+                  boxShadow:
+                    "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                  padding: "30px 20px",
                 }}
-                aria-label="breadcrumbs"
               >
-                {/* <LinkIcon /> */}
-                <Link
-                  style={{ color: "black", textDecoration: "none" }}
-                  color="black"
-                  to="/listen"
+                <Breadcrumbs
+                  sx={{
+                    marginLeft: "20px",
+                    fontSize: "12px",
+                    marginBottom: "10px",
+                    color: "black",
+                    textDecoration: "none",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  aria-label="breadcrumbs"
                 >
-                  Listen Practice
-                </Link>
+                  {/* <LinkIcon /> */}
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    color="black"
+                    to="/listen"
+                  >
+                    Listen Practice
+                  </Link>
 
-                <Link
-                  style={{ color: "black", textDecoration: "none" }}
-                  color="black"
-                  to="/listen"
-                >
-                  {lesson?.listenGroup.name}
-                </Link>
-                <Link
-                  style={{ color: "black", textDecoration: "none" }}
-                  to={`/listen/${lesson?.id}`}
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    color="black"
+                    to="/listen"
+                  >
+                    {lesson?.listenGroup.name}
+                  </Link>
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    to={`/listen/${lesson?.id}`}
+                  >
+                    {lesson?.name}
+                  </Link>
+                </Breadcrumbs>
+                <Typography
+                  sx={{ marginBottom: "40px", marginLeft: "20px" }}
+                  variant="h4"
                 >
                   {lesson?.name}
-                </Link>
-              </Breadcrumbs>
-              <Typography
-                sx={{ marginBottom: "40px", marginLeft: "20px" }}
-                variant="h4"
-              >
-                {lesson?.name}
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "4px",
-                  alignItems: "center",
-                  marginBottom: "20px",
-                  marginLeft: "20px",
-                }}
-              >
-                <Typography variant="h5">
-                  Sentence: {currIndex} / {lesson?.listenSentences.length}
                 </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "4px",
-                  alignItems: "center",
-                  marginBottom: "20px",
-                  marginLeft: "20px",
-                }}
-              >
-                <audio key={currIndex} controls>
-                  <source
-                    src={lesson?.listenSentences[currIndex - 1]?.audio || ""}
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4px",
+                    alignItems: "center",
+                    marginBottom: "20px",
+                    marginLeft: "20px",
+                  }}
+                >
+                  <Typography variant="h5">
+                    Sentence: {currIndex} / {lesson?.listenSentences.length}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4px",
+                    alignItems: "center",
+                    marginBottom: "20px",
+                    marginLeft: "20px",
+                  }}
+                >
+                  <audio key={currIndex} controls>
+                    <source
+                      src={lesson?.listenSentences[currIndex - 1]?.audio || ""}
+                    />
+                    Your browser does not support the audio element.
+                  </audio>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4px",
+                    alignItems: "center",
+                    marginLeft: "2px",
+                  }}
+                >
+                  <TextField
+                    label="Type your answer"
+                    sx={{ m: 1, width: "100%" }}
+                    // color="secondary"
+                    // color="success"
+                    color={answerState[currIndex - 1].state}
+                    focused
+                    rows={2}
+                    multiline
+                    value={answer[currIndex - 1]}
+                    onChange={handleChangeInput}
                   />
-                  Your browser does not support the audio element.
-                </audio>
-              </Box>
-              <Box
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "4px",
+                    alignItems: "center",
+                    marginLeft: "20px",
+                    marginBottom: "20px",
+                    minHeight: "60px",
+                  }}
+                >
+                  <TipsAndUpdatesIcon
+                    sx={{ width: "18px", height: "18px" }}
+                  ></TipsAndUpdatesIcon>
+                  {showAnswer ? (
+                    <>
+                      <Typography variant="h6">Answer:</Typography>
+                      <Typography variant="h6">
+                        {lesson?.listenSentences[currIndex - 1]?.sentence}
+                      </Typography>
+                    </>
+                  ) : (
+                    <>
+                      <Typography variant="h6">Check:</Typography>
+                      {answerState[currIndex - 1].checked &&
+                        sentenceCheck[currIndex - 1].map((sentence, index) => {
+                          return (
+                            <Typography
+                              key={index}
+                              color={sentence.state}
+                              variant="h6"
+                            >
+                              {sentence.sentence}
+                            </Typography>
+                          );
+                        })}
+                    </>
+                  )}
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: "8px",
+                    alignItems: "center",
+                    marginLeft: "20px",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    sx={{ background: "#478ECC" }}
+                    startIcon={<NavigateBeforeIcon />}
+                    onClick={handlePrevSentence}
+                    disabled={currIndex == 1}
+                  >
+                    Prev
+                  </Button>
+                  <Button
+                    onClick={handleCheckAnswer}
+                    variant="outlined"
+                    sx={{ color: "#203a90", borderColor: "#203a90" }}
+                  >
+                    Check
+                  </Button>
+                  <Button onClick={handleShowAnswer} variant="contained">
+                    Answer
+                  </Button>
+                  {currIndex >= (lesson?.listenSentences.length ?? 0) ? (
+                    <Button
+                      variant="contained"
+                      sx={{ background: "#478ECC" }}
+                      endIcon={<DoneIcon />}
+                      onClick={() => setMode("overview")}
+                    >
+                      Finish
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="contained"
+                      sx={{ background: "#478ECC" }}
+                      endIcon={<NavigateNextIcon />}
+                      onClick={handleNextSentence}
+                    >
+                      Next
+                    </Button>
+                  )}
+                </Box>
+              </Card>
+            ) : (
+              <Card
                 sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "4px",
-                  alignItems: "center",
-                  marginLeft: "2px",
+                  width: "80%",
+                  marginLeft: "80px",
+                  height: "70%",
+                  minHeight: "480px",
+                  boxShadow:
+                    "rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px",
+                  padding: "30px 20px",
                 }}
               >
-                <TextField
-                  label="Type your answer"
-                  sx={{ m: 1, width: "100%" }}
-                  // color="secondary"
-                  // color="success"
-                  color={answerState[currIndex - 1].state}
-                  focused
-                  rows={2}
-                  multiline
-                  value={answer[currIndex - 1]}
-                  onChange={handleChangeInput}
-                />
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "4px",
-                  alignItems: "center",
-                  marginLeft: "20px",
-                  marginBottom: "20px",
-                  minHeight: "60px",
-                }}
-              >
-                <TipsAndUpdatesIcon
-                  sx={{ width: "18px", height: "18px" }}
-                ></TipsAndUpdatesIcon>
-                {showAnswer ? (
-                  <>
-                    <Typography variant="h6">Answer:</Typography>
-                    <Typography variant="h6">
-                      {lesson?.listenSentences[currIndex - 1]?.sentence}
-                    </Typography>
-                  </>
-                ) : (
-                  <>
-                    <Typography variant="h6">Check:</Typography>
-                    {answerState[currIndex - 1].checked &&
-                      sentenceCheck[currIndex - 1].map((sentence, index) => {
-                        return (
-                          <Typography
-                            key={index}
-                            color={sentence.state}
-                            variant="h6"
-                          >
-                            {sentence.sentence}
-                          </Typography>
-                        );
-                      })}
-                  </>
-                )}
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "row",
-                  gap: "8px",
-                  alignItems: "center",
-                  marginLeft: "20px",
-                }}
-              >
-                <Button
-                  variant="contained"
-                  sx={{ background: "#478ECC" }}
-                  startIcon={<NavigateBeforeIcon />}
-                  onClick={handlePrevSentence}
-                  disabled={currIndex == 1}
+                <Breadcrumbs
+                  sx={{
+                    marginLeft: "20px",
+                    fontSize: "12px",
+                    marginBottom: "10px",
+                    color: "black",
+                    textDecoration: "none",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  aria-label="breadcrumbs"
                 >
-                  Prev
-                </Button>
-                <Button
-                  onClick={handleCheckAnswer}
-                  variant="outlined"
-                  sx={{ color: "#203a90", borderColor: "#203a90" }}
+                  {/* <LinkIcon /> */}
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    color="black"
+                    to="/listen"
+                  >
+                    Listen Practice
+                  </Link>
+
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    color="black"
+                    to="/listen"
+                  >
+                    {lesson?.listenGroup.name}
+                  </Link>
+                  <Link
+                    style={{ color: "black", textDecoration: "none" }}
+                    to={`/listen/${lesson?.id}`}
+                  >
+                    {lesson?.name}
+                  </Link>
+                </Breadcrumbs>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: "20px",
+                  }}
                 >
-                  Check
-                </Button>
-                <Button onClick={handleShowAnswer} variant="contained">
-                  Answer
-                </Button>
-                <Button
-                  variant="contained"
-                  sx={{ background: "#478ECC" }}
-                  endIcon={<NavigateNextIcon />}
-                  onClick={handleNextSentence}
-                  disabled={currIndex >= (lesson?.listenSentences.length ?? 0)}
-                >
-                  Next
-                </Button>
-              </Box>
-            </Card>
+                  <Typography
+                    sx={{ marginBottom: "20px" }}
+                    variant="h4"
+                    align="center"
+                  >
+                    You have completed this exercise, good job!
+                  </Typography>
+                  <CheckCircleIcon
+                    sx={{
+                      width: "100px",
+                      height: "100px",
+                      margin: "auto",
+                      color: "#198754",
+                    }}
+                  ></CheckCircleIcon>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      gap: "8px",
+                      alignItems: "center",
+                      marginLeft: "20px",
+                      justifyContent: "center",
+                      marginTop: "20px",
+                    }}
+                  >
+                    {lesson?.prev && (
+                      <Button
+                        variant="outlined"
+                        sx={{
+                          background: "white",
+                          color: "#6C757D",
+                          borderColor: "#6C757D",
+                          "&:hover": {
+                            opacity: "0.8",
+                          },
+                        }}
+                        onClick={() => {
+                          if (lesson.prev) {
+                            window.location.href = `/listen/${lesson.prev}`;
+                          }
+                        }}
+                      >
+                        Previous exercise
+                      </Button>
+                    )}
+                    <Button
+                      variant="outlined"
+                      sx={{
+                        background: "white",
+                        color: "#6C757D",
+                        borderColor: "#6C757D",
+                        "&:hover": {
+                          opacity: "0.8",
+                        },
+                      }}
+                      onClick={() => window.location.reload()}
+                    >
+                      Repeat this exercise
+                    </Button>
+                    {lesson?.next && (
+                      <Button
+                        variant="contained"
+                        sx={{
+                          background: "#0D6EFD",
+                          color: "white",
+                          "&:hover": {
+                            opacity: "0.8",
+                          },
+                        }}
+                        onClick={() => {
+                          if (lesson.next) {
+                            window.location.href = `/listen/${lesson.next}`;
+                          }
+                        }}
+                      >
+                        Next exercise
+                      </Button>
+                    )}
+                  </Box>
+                </Box>
+              </Card>
+            )}
             <Box
               sx={{
                 height: "70%",
