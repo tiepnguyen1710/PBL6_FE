842a3a8fbba9b96e0a7fae7eccef03a4dbdce924
TiepNguyen <tiepnguyen.17102003@gmail.com>
2024-12-25 23:13:04 +0700

    info user box

--- src/features/toeic-exam/components/InforUserBox.tsx
+++ src/features/toeic-exam/components/InforUserBox.tsx
@@ -1,43 +1,85 @@
 import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import userIcon from "../assets/user_icon.webp";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../stores";
+import { AuthState } from "../../../stores/authSlice";
+import { me } from "../../auth/api/account-api";
+import { useQuery } from "@tanstack/react-query";
+import { useCallback, useMemo } from "react";
+import { differenceInDays, format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const InforUserBox = () => {
+  const navigate = useNavigate();
+  const { token } = useSelector<RootState, AuthState>((state) => state.auth);
+
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => me(token!),
+  });
+
+  const getDefaultTestDate = useCallback(() => {
+    let createdDate = new Date();
+    if (user) {
+      createdDate = new Date(user.createdAt);
+    }
+    const defaultTestDate = createdDate;
+    defaultTestDate.setMonth(defaultTestDate.getMonth() + 3);
+
+    return defaultTestDate;
+  }, [user]);
+
+  const userTestDate = useMemo(() => {
+    return user?.testDate ? new Date(user.testDate) : getDefaultTestDate();
+  }, [user?.testDate, getDefaultTestDate]);
   return (
     <>
-      <Box sx={{ mb: 1 }}>
-        <Stack direction="column" alignItems="center" justifyContent="center">
-          <span style={{ width: "50px", height: "auto" }}>
-            <img src={userIcon} />
-          </span>
-          <Typography variant="h6">TienZe</Typography>
-        </Stack>
-      </Box>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <>
+          <Box sx={{ mb: 1 }}>
+            <Stack
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <span style={{ width: "50px", height: "auto" }}>
+                <img src={user?.avatar ?? userIcon} />
+              </span>
+              <Typography variant="h6">{user?.name}</Typography>
+            </Stack>
+          </Box>
 
-      <Divider />
+          <Divider />
 
-      <Box sx={{ mt: 1 }}>
-        <Stack direction="column" spacing={0.25}>
-          <Stack direction="row" justifyContent={"space-between"}>
-            <Typography>Days until exam</Typography>
-            <Typography color="secondary.dark" fontWeight="bold">
-              111 days
-            </Typography>
-          </Stack>
-          <Stack direction="row" justifyContent={"space-between"}>
-            <Typography>Exam day</Typography>
-            <Typography color="secondary.dark" fontWeight="bold">
-              31/12/2024
-            </Typography>
-          </Stack>
-          <Stack direction="row" justifyContent={"space-between"}>
-            <Typography>Target score</Typography>
-            <Typography color="secondary.dark" fontWeight="bold">
-              800
-            </Typography>
-          </Stack>
-          <Button variant="contained">Analysis your result</Button>
-        </Stack>
-      </Box>
+          <Box sx={{ mt: 1 }}>
+            <Stack direction="column" spacing={0.5}>
+              <Stack direction="row" justifyContent={"space-between"}>
+                <Typography>Days until exam</Typography>
+                <Typography color="secondary.dark" fontWeight="bold">
+                  {`${differenceInDays(userTestDate, new Date())} days`}
+                </Typography>
+              </Stack>
+              <Stack direction="row" justifyContent={"space-between"}>
+                <Typography>Exam day</Typography>
+                <Typography color="secondary.dark" fontWeight="bold">
+                  {format(userTestDate, "dd/MM/yyyy")}
+                </Typography>
+              </Stack>
+              <Stack direction="row" justifyContent={"space-between"}>
+                <Typography>Target score</Typography>
+                <Typography color="secondary.dark" fontWeight="bold">
+                  {user?.targetScore.toString() || "450"}
+                </Typography>
+              </Stack>
+              <Button variant="contained" onClick={() => navigate("/history")}>
+                Analysis your result
+              </Button>
+            </Stack>
+          </Box>
+        </>
+      )}
     </>
   );
 };
